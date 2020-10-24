4781b1e2bc7bd55191e581ac974144ca5adbdcb9
Concinnity888 <pogorelova_ann@mail.ru>
2020-10-25 02:01:21 +0500

    feat: add calendar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import Calendar from './components/Calendar';
+
 class App extends React.Component {
+  state = {
+    date: null
+  };
+
+  handleDateChange = (date) => this.setState({ date });
+
   render() {
+    const { date } = this.state;
+
     return (
       <div>
+        { date && <p>Выбранная дата: {date.toLocaleDateString()}</p>}
+
+        <Calendar
+          onChange={this.handleDateChange}
+        />
       </div>
     );
   }
